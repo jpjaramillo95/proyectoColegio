ef0b144f354950ead7b87589b3898f5ad7a0d5da
Juan Pablo Jaramillo G. <juanpjaramillo31@gmail.com>
2025-05-14 23:18:13 -0500

    feat se crea banner

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "flowbite-react";
 export default function Banner() {
   return (
     <>
-    <div className="h-90 pt-5">
+    <div className="h-90 pt-4">
       <Carousel>
         <img src="../../../src/assets/img/banner1.jpg" alt="..." />
         {/* <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." /> */}
