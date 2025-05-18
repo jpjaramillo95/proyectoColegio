91d307ea87ec47dd205bafafd4c30feb975c2dfd
Juan Pablo Jaramillo G. <juanpjaramillo31@gmail.com>
2025-05-18 10:07:25 -0500

    feat se finaliza el home

--- src/components/common/CardNoticias.jsx
+++ src/components/common/CardNoticias.jsx
@@ -9,7 +9,7 @@ export default function CardNoticias() {
         <img src={noticia1} alt="img noticia" className="imgNoticia" />
         <h2 className='tituloNoticia'>Comienzan los parciales</h2>
         <div className="fechaNoticia">17 May 2025</div>
-        <p className=''> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rem corporis a enim sapiente temporibus quae illo quis molestias dolores, totam, maiores quibusdam numquam quod esse quasi ipsa provident recusandae!</p>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias rem corporis a enim sapiente temporibus quae illo quis molestias dolores, totam, maiores quibusdam numquam quod esse quasi ipsa provident recusandae!</p>
         <button className='btnNoticia'>Leer más</button>
       </div>
     </>
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -4,6 +4,10 @@ import banner1 from "../../../../src/assets/img/banner1.jpg"
 import banner2 from "../../../../src/assets/img/banner2.jpg"
 import btnDerecho from "../../../../src/assets/img/btnDer.svg"
 import btnIzquierdo from "../../../../src/assets/img/btnIzq.svg"
+import iconoGraduado from "../../../../src/assets/img/graduadosIconoai.svg"
+import iconoEstudiante from "../../../../src/assets/img/estudianteIcono-01.svg"
+import iconoProfesor from "../../../../src/assets/img/profesorIcono-01.svg"
+import iconoExperiencia from "../../../../src/assets/img/experienciaIcono-01.svg"
 import CardNoticias from "../../common/CardNoticias"
 
 export default function Home() {
@@ -48,13 +52,30 @@ export default function Home() {
           <CardNoticias></CardNoticias>
         </div>
         <div className='contenedorCifras'>
-          <h1>Nuestro Colegio En Cifras</h1>
-          <h3>Nuestras estadisticas</h3>
-          <div className='contenedorCifras'>
+          <div className='tituloCifras'>
+            <h1>Nuestro Colegio En Cifras</h1>
+          </div>
+          <h2>Nuestras estadisticas</h2>
+          <div className='contenedorEstadisticas'>
             <div className='cifras'>
-              <img src="" alt="" />
+              <img src={iconoEstudiante} alt="" className='imgEstadisticas' />
               <p className="cifra">204</p>
               <p className="dato">ESTUDIANTES</p>
+            </div>
+            <div className='cifras'>
+              <img src={iconoGraduado} alt="" className='imgEstadisticas' />
+              <p className="cifra">996</p>
+              <p className="dato">EGRESADOS</p>
+            </div>
+            <div className='cifras'>
+              <img src={iconoProfesor} alt="" className='imgEstadisticas' />
+              <p className="cifra">16</p>
+              <p className="dato">DIRECTIVOS Y DOCENTES</p>
+            </div>
+            <div className='cifras'>
+              <img src={iconoExperiencia} alt="" className='imgEstadisticas' />
+              <p className="cifra">20</p>
+              <p className="dato">AÑOS DE EXPERIENCIA</p>
             </div>
           </div>
         </div>
